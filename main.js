d015070aae72b2d1e9daa216100f9ea1fbfd5dc6
LaylaMSantos <162986281+LaylaMSantos@users.noreply.github.com>
2024-09-11 15:11:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const nomeComIPT = document.getElementById("nomeComlpetoInput");
+const validarBT = document.getElementById("validarBT");
+const saidaUsuario = document.getElementById("saidaRegex");
+
+//Validador de Nome Completo
+const nomeCompRegex = /^[a-zA-Z\s]{2,}$/
+
+validarBT.addEventListener("click", (e) => {
+    if(nomeCompRegex.test(nomeComIPT.value)){
+        saidaUsuario.innerHTML = "Hehe"
+        saidaUsuario.style.color = "green"
+    } else {
+        saidaUsuario.innerHTML = "Not Hehe"
+        saidaUsuario.style.color = "red"
+    }
+})
